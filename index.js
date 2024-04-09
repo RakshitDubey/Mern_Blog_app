4f6fc525431956ebd803287260add2b71ed216b7
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2024-04-09 05:34:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const connectDB=async()=>{
 
 app.use('/images',express.static(path.join(__dirname,'/images')))
 
-app.use(cors())
+app.use(cors({
+    origin:"https://mern-blog-frontend-ruby.vercel.app/"
+}))
 dotenv.config()
 
 app.use(cookieparser())
